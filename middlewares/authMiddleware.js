dc1fbf5fffd45c4ce446efa80fe6a62bb1e8e74b
Charlene149611 <c.gausset911@gmail.com>
2025-07-12 23:41:32 +0200

    implémentation gestion utilisateurs: contrôle rôles, token JWT, liste utilisateurs, optimisation protection des routes, création de profile.twig

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -8,8 +8,9 @@ export function protect(req, res, next) {
 
   // Récupère le token dans l'en-tête Authorization OU dans les cookies
   const authHeader = req.headers["authorization"];
-  const token =
-    authHeader?.split(" ")[1];
+  const tokenFromHeader = authHeader?.split(" ")[1];
+  const tokenFromCookie = req.cookies?.token;
+  const token = tokenFromHeader || tokenFromCookie;
 
   if (!token) {
     return res.status(401).json({ message: "Token requis" });
@@ -18,11 +19,12 @@ export function protect(req, res, next) {
   // Vérifie le token
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
+      console.error("Token invalide ou expiré", err);
       return res.status(403).json({ message: "Token invalide" });
     }
 
     req.user = user; // Ajoute l'utilisateur décodé à la requête
-    console.log("Token vérifié avec succès");
+    console.log("Token vérifié avec succès", user);
     next();
   });
 }
